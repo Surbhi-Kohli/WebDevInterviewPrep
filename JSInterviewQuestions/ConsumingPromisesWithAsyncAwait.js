d9e485d0f2e40eec33d7e22cada214b6735b43e0
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-12-04 00:15:47 +0530

    Update ConsumingPromisesWithAsyncAwait.js

--- JSInterviewQuestions/ConsumingPromisesWithAsyncAwait.js
+++ JSInterviewQuestions/ConsumingPromisesWithAsyncAwait.js
@@ -24,5 +24,28 @@ const whereAmI2=async function(country){
  const data=await res.json();
  console.log(data);
  console.log(data[0]);
- 
 }
+
+/*  Chaining promises with async await*/
+function abc2(seconds){
+return new Promise((resolve)=>{
+setTimeout(resolve,seconds*1000);
+ });
+}
+
+const whereAmI2 = async function(country){
+ let res= await abc2(5);
+ console.log("hey after 5 seconds");
+ console.log("next in line");
+ await abc2(4);
+ console.log("hey after approx 10 seconds")
+
+}
+whereAmI2();
+console.log("First Me");
+//Output:
+// First Me
+// hey after 5 seconds
+// next in line
+/* All lines post await are like chained then() blocks whether they inturn await promises or not  */
+// hey after approx 10 seconds
