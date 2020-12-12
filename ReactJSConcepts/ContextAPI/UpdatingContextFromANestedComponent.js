20412f7977fc43e78919a4696f4e8ea235c9429b
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-12-12 18:00:39 +0530

    Create UpdatingContextFromANestedComponent.js

---
+++ ReactJSConcepts/ContextAPI/UpdatingContextFromANestedComponent.js
@@ -0,0 +1,84 @@
+/*   It is often necessary to update the context from a component that is nested somewhere deeply in the component tree. 
+      In this case you can pass a function down through the context to allow consumers to update the context:
+
+theme-context.js */
+
+// Make sure the shape of the default value passed to
+// createContext matches the shape that the consumers expect!
+export const ThemeContext = React.createContext({
+  theme: themes.dark,
+  toggleTheme: () => {},
+});
+
+//-----------------------------------------------------------//
+theme-toggler-button.js
+//----------------------------------------------------------//
+import {ThemeContext} from './theme-context';
+
+function ThemeTogglerButton() {
+  // The Theme Toggler Button receives not only the theme
+  // but also a toggleTheme function from the context
+  return (
+    <ThemeContext.Consumer>
+      {({theme, toggleTheme}) => (
+        <button
+          onClick={toggleTheme}
+          style={{backgroundColor: theme.background}}>
+          Toggle Theme
+        </button>
+      )}
+    </ThemeContext.Consumer>
+  );
+}
+
+export default ThemeTogglerButton;
+/----------------------------------------------------------------------------------------------------------------------------------------------------------------------/
+app.js
+
+import {ThemeContext, themes} from './theme-context';
+import ThemeTogglerButton from './theme-toggler-button';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark
+            ? themes.light
+            : themes.dark,
+      }));
+    };
+
+    // State also contains the updater function so it will
+    // be passed down into the context provider
+    this.state = {
+      theme: themes.light,
+      toggleTheme: this.toggleTheme,
+    };
+  }
+
+  render() {
+    // The entire state is passed to the provider
+    return (
+      <ThemeContext.Provider value={this.state}>
+        <Content />
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+function Content() {
+  return (
+    <div>
+      <ThemeTogglerButton />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.root);
+
+
+
+
