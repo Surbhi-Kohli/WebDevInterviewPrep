08a226464c1163cf8ad7aa5a836b5290027357d3
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-12-19 14:25:25 +0530

    Update Polyfills.js

--- JSInterviewQuestions/Polyfills.js
+++ JSInterviewQuestions/Polyfills.js
@@ -131,3 +131,35 @@ const throttle = (func, limit) => {
     }
   }
 }
+
+/**********************************    Promise.all  ********************************************************/
+
+function allPromise(promises) {
+  // return a new promise
+  return new Promise((resolve, reject) => {
+    // check promises count
+    var promiseCount = promises.length;
+    var promisedData = [];
+    var settledCount = 0;
+
+    // Check the status
+
+    promises.forEach((promise, i) => {
+      promise
+        .then((data) => {
+          // pushed in the array and increment count
+          promisedData[i] = data;
+          settledCount++;
+          // check the promise count and settledCount
+          if (settledCount === promiseCount) {
+            resolve(promisedData);
+          }
+        })
+        .catch((error) => {
+          // if promise fail then reject
+          reject(error);
+        });
+    });
+  });
+}
+
