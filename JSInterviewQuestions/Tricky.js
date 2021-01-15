853b6ba3829a5bd56a29fa52fa947e4233c3e07d
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-15 23:18:07 +0530

    Create Tricky.js

---
+++ JSInterviewQuestions/Tricky.js
@@ -0,0 +1,10 @@
+var person = {};
+
+({ name: person["username"] } = {
+	username: "yomeshgupta",
+	email: "team@devtools.tech",
+	name: "yomesh",
+});
+
+console.log(person.username, person.name);
+//yomesh undefined
