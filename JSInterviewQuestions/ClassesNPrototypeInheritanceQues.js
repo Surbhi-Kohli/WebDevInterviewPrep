a4aa8cc6ac74ce4abf8bcd4a063ded0a73eba100
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-15 20:45:43 +0530

    Create ClassesNPrototypeInheritanceQues.js

---
+++ JSInterviewQuestions/ClassesNPrototypeInheritanceQues.js
@@ -0,0 +1,24 @@
+/*Question 1*/
+var Duryodhana = new Kaurav('Duryodhana', 'Gada')
+let Arjuna = new Pandav('Arjuna', 'Gandiva')
+const Dronacharya = new Guru('Dronacharya')
+console.log(Duryodhana);
+console.log(
+  Arjuna.weapon,
+  window.Arjuna,
+  Duryodhana.weapon,
+  window.Duryodhana,
+  Dronacharya.name,
+  window.Dronacharya
+)
+/* console.log(Duryodhana)
+{
+  name: "Duryodhana",
+  weapon: "Gada"
+}
+//2nd console.log()
+"Gandiva", undefined, "Gada", {
+  name: "Duryodhana",
+  weapon: "Gada"
+}, "Dronacharya", undefined 
+*/
