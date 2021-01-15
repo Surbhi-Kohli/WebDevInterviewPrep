c0901fe3f8f3601adc99717a519f967d9af5ab98
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-15 20:38:06 +0530

    Update ExoticArrayObject.js

--- JSInterviewQuestions/ExoticArrayObject.js
+++ JSInterviewQuestions/ExoticArrayObject.js
@@ -4,5 +4,5 @@ console.log(arr.length);//0
 console.log(arr.null);//foo
 console.log(arr[null]);//foo
 arr[5]="meow";  
-console.log(arr.length);
-console.log(arr);
+console.log(arr.length);//6
+console.log(arr); //[undefined, undefined, undefined, undefined, undefined, "meow"]
