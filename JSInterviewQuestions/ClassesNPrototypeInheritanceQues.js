adae89a11058a5487558f01be752b1465c037c6b
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-16 00:10:42 +0530

    Update ClassesNPrototypeInheritanceQues.js

--- JSInterviewQuestions/ClassesNPrototypeInheritanceQues.js
+++ JSInterviewQuestions/ClassesNPrototypeInheritanceQues.js
@@ -47,3 +47,33 @@ EXPLANATION: Final object would look like
 While searching for property name, first look up would be on the direct enumerable properties of the object and since that exists (this.name = "Yomesh"),
 it would be printed. If no direct property is present then Prototype chain is traversed and in that case value would be Saloni. 
 In case of channel property, Prototype chain is traversed and value is printed.*/
+
+/*Question 3 Hint:Returning Non-primitive Value from a constructor*/
+function parseData(data) { /* Some computation */ return data;  };
+
+class User {
+	constructor(name, ...data) {
+		const parsedData = parseData(data);
+
+		this.name = name;
+		this.data = parseData;
+
+		return parseData;
+	}
+
+	getData() {
+		return this.data;
+	}
+}
+
+const Yomesh = new User('Yomesh', { youtubeChannel: 'https://bit.ly/devtools-yt', shouldSubscribe: true });
+
+// What would be the output of the following statement?
+console.log(Yomesh.getData()); 
+
+/*A.{ youtubeChannel: 'https://bit.ly/devtools-yt', shouldSubscribe: true } B.undefined C.Error D.null E.[object Object]
+ans-c
+EXPLANATION:Class in JavaScript is (mostly) just syntactical sugar. The underlying working is same as the constructor function. 
+Since, in the code snippet we are creating an instance, using the new operator, 
+but returning a non-primitive value from the constructor so implicit this will be lost and instance won't have any getData method to call.
+  */
