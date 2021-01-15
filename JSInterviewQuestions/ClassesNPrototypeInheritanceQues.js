b40654e47b6852ed64052014d0c09685d88e8448
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-15 23:34:25 +0530

    Update ClassesNPrototypeInheritanceQues.js

--- JSInterviewQuestions/ClassesNPrototypeInheritanceQues.js
+++ JSInterviewQuestions/ClassesNPrototypeInheritanceQues.js
@@ -22,3 +22,28 @@ console.log(
   weapon: "Gada"
 }, "Dronacharya", undefined 
 */
+/*Question 2*/
+var name = "Puneet";
+
+function Engineer() {
+	this.name = "Yomesh";
+}
+
+console.log(new Engineer().name);
+Engineer.prototype.name = "Saloni";
+Engineer.prototype.channel = "https://bit.ly/devtools-yt";
+console.log(new Engineer().channel);
+console.log(new Engineer().name);
+
+/*A.Puneet https://bit.ly/devtools-yt  Yomesh  B.Puneet https://bit.ly/devtools-yt  Saloni  C.Yomesh https://bit.ly/devtools-yt Yomesh
+D. Puneet undefined Yomesh   E.Puneet
+https://bit.ly/devtools-yt
+Puneet
+
+ans-C
+EXPLANATION: Final object would look like
+
+{ name: "Yomesh", __proto__: { name: "Saloni", channel: "https://bit.ly/devtools-yt" } }
+While searching for property name, first look up would be on the direct enumerable properties of the object and since that exists (this.name = "Yomesh"),
+it would be printed. If no direct property is present then Prototype chain is traversed and in that case value would be Saloni. 
+In case of channel property, Prototype chain is traversed and value is printed.*/
