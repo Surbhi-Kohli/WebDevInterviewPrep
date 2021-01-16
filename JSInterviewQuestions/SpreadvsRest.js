84a5a09c3fd18c4c44b2a6a36afbc979c7a016f4
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-16 21:46:00 +0530

    Update SpreadvsRest.js

--- JSInterviewQuestions/SpreadvsRest.js
+++ JSInterviewQuestions/SpreadvsRest.js
@@ -1,1 +1,4 @@
 /*Spread syntax "expands" an array into its elements, while rest syntax collects multiple elements and "condenses" them into a single element*/
+const clone = {...original} to shallow clone
+
+const newobj = {...original, prop: newOne} to immutably add another prop to the original and store as a new object.
