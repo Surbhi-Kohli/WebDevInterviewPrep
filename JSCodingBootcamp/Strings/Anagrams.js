4ff0eb01b2e10b1fcac89f1bf7a015674dc07c49
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-11 15:09:05 +0530

    Update Anagrams.js

--- JSCodingBootcamp/Strings/Anagrams.js
+++ JSCodingBootcamp/Strings/Anagrams.js
@@ -1,1 +1,31 @@
+function  anagram(str1,str2)
+{
+let obj={};
+if(str1.length!=str2.length)
+ return false;
 
+ for(let s of str1)
+ {
+    obj[s]=obj[s]+1||1;
+ }
+ for(let s of str2)
+ {
+   if(obj[s])
+   {
+   obj[s]-=1;
+   }
+   else 
+   return false;
+ }
+for(let o in obj)
+{
+  if(obj[o]!=0)
+  return false;
+}
+return true;
+}
+let stt='abc';
+let str2='bac';
+console.log(anagram(stt,str2));//true
+let str1="aabac";
+console.log(anagram(str1,str2));//false
