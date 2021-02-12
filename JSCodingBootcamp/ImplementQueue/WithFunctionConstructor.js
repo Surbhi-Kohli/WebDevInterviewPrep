7ed2980c95cf4029ecfabc4840bc768e09c01f03
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-12 18:43:19 +0530

    Update WithFunctionConstructor.js

--- JSCodingBootcamp/ImplementQueue/WithFunctionConstructor.js
+++ JSCodingBootcamp/ImplementQueue/WithFunctionConstructor.js
@@ -1,21 +1,27 @@
-function Queue {
-     this.arr=[];
-    this.addVal= add(val){
-     this.arr.push(val);
-    };
- this.removeVal= remove(){
-     return  this.arr.shift();
+function Queue(){
+     let arr=[];
+    arr.add =(val)=>{
+     arr.push(val);
+    };  
+ arr.remove=()=>{
+     return  arr.shift();
  };
- 
+ return arr;//returning non-primitive value-->>'this' will be a non primitive 
+ //value we return here
 }
 
 let q=new Queue();
+console.log(q); //[]
+//console.log(q.add);
 q.add(1);
 q.add(2);
 q.add(3);
 q.add(4);
-console.log(q);
+console.log(q); // [1, 2, 3, 4]
 q.remove();
-console.log(q);
+console.log(q);// [2, 3, 4]
 q.remove();
-console.log(q);
+console.log(q); //[3, 4]
+
+
+
