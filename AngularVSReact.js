deb502727edce74b8fe36ca368904f1fb543d218
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-16 17:40:03 +0530

    Create AngularVSReact.js

---
+++ AngularVSReact.js
@@ -0,0 +1,21 @@
+/*AngularJS (1.x) approaches building an application by extending HTML markup and injecting various constructs (e.g. Directives, Controllers, Services) at runtime.
+    As a result, AngularJS is very opinionated about the greater architecture of your application — these abstractions are certainly useful in 
+some cases, but in many situations, they come at the cost of flexibility.
+
+
+By contrast, React focuses exclusively on the creation of components, and has few (if any) opinions about an application’s architecture. 
+This allows a developer an incredible amount of flexibility in choosing the architecture they deem “best” —
+though it also places the responsibility of choosing (or building) those parts on the developer.
+
+
+React enables developers to render a user interface. To create a full front-end application, developers need other pieces, 
+such as state management tools like Redux.
+
+Like React, Angular enables developers to render a user interface, but it is a “batteries included” framework that includes prescriptive,
+opinionated solutions to common requirements like state management.
+
+While there are many other considerations when comparing React and Angular specifically, this key architectural difference means that:
+
+Using a library such as React can give a project a greater ability to evolve parts of the system—again for example, state management—over time,
+when new solutions are created by the open source community.
+Using a framework such as Angular can make it easier for developers to get started and can also simplify maintenance.*/
