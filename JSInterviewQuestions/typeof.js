26199b9dd5de74bd55daadd4bbc88a9d7cabc6c3
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-17 02:01:38 +0530

    Create typeof.js

---
+++ JSInterviewQuestions/typeof.js
@@ -0,0 +1,12 @@
+function employeeId() {
+		console.log(typeof employeeId);
+	}
+ 
+console.log(typeof employeeId); //function
+let abc="ncjdn";
+console.log(typeof abc);//string
+console.log(typeof undeclaredVar);//undefined
+let nullVar=null;
+console.log(typeof nullVar);//Object
+let numberVar=11;
+console.log(typeof numberVar);//number
