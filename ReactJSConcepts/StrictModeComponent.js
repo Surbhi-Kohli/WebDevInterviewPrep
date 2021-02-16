f9f3af4018e099ca8ff6d8cb0e05faeb28f04cf1
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-16 15:23:19 +0530

    Create StrictModeComponent.js

---
+++ ReactJSConcepts/StrictModeComponent.js
@@ -0,0 +1,8 @@
+/*What is the StrictMode component and why would you use it?
+<StrictMode /> is a component included with React to provide additional visibility of potential issues in components. 
+If the application is running in development mode, any issues are logged to the development console, but these warnings are not shown if the application 
+is running in production mode.
+
+Developers use <StrictMode /> to find problems such as deprecated lifecycle methods and legacy patterns, to ensure that all React components follow current best practices.
+
+<StrictMode /> can be applied at any level of an application component hierarchy, which allows it to be adopted incrementally within a codebase.*/
