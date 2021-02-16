81de9bfaae7b78f6defe5fc12867ce01f2f9209c
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-16 19:34:56 +0530

    Update React.cloneElement.js

--- ReactJSConcepts/React.cloneElement.js
+++ ReactJSConcepts/React.cloneElement.js
@@ -1,8 +1,8 @@
 /*
 props.children isn't the actual children; It is the descriptor of the children. So you don't have actually 
-anything to change; you can't change any props, or edit any functionality; you can only read from it. If you need to make any modifications you
+anything to change; you can't change any props, or edit any functionality; you can only read from it. If you need to make any modifications or extend the functionality
+of children components,you
 have to create new elements using React.CloneElement.
-
 https://egghead.io/lessons/react-use-react-cloneelement-to-extend-functionality-of-children-components
 
 An example:
@@ -16,4 +16,24 @@ render() {
               <Sentence>Third</Sentence>
             </Paragraph>   
     ) 
+}
+/**Buttons*/
+function Buttons(props)
+{ const [selected,setSelected]=useState('None');
+ let selectItem=(val)=>{
+     setSelected(val);
+ }
+ let cb=(child)=>{
+     //as props are read only,so each child from props.children is readonly,so to add functionality to children,we actually need to clone them
+     React.cloneElement(child,{
+      onClick:(child)=>selectItem(child.value)   
+     })
+ }
+ let items=React.Children.map(props.children,cb);
+    return(
+     <div>
+        <h2>You have selected {selected}</h2>
+        {items}
+        </div>
+    )
 }
