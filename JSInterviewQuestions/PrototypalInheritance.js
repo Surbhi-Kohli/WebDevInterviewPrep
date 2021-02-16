3fa244c14f64f19c66e30d5c09a7510370bacac9
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-02-17 00:35:08 +0530

    Update PrototypalInheritance.js

--- JSInterviewQuestions/PrototypalInheritance.js
+++ JSInterviewQuestions/PrototypalInheritance.js
@@ -273,5 +273,16 @@ Employee.prototype.JobId = '1BJKSJ';
 console.log(new Employee().JobId);//1BJKSJ
 console.log(new Employee().employeeId); //bq1uy as when the object has that property,no need to check the value from prototype
 
+/**************************Question 11**********************************************/
+//What will be the output of the following code?
+var Employee = {
+  company: 'xyz'
+}
+var emp1 = Object.create(Employee);
+delete emp1.company
+console.log(emp1.company);
 
-*/
+/*The code above will output `xyz` as output. Here `emp1` object got company as **prototype** property. delete operator doesn't delete prototype property.
+emp1 object doesn't have company as its own property. you can test it console.log(emp1.hasOwnProperty('company')); //output : false However, we can delete 
+company property directly from Employee object using 
+delete Employee.company or we can also delete from emp1 object using __proto__ property delete emp1.__proto__.company.*/
