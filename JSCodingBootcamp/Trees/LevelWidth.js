db81a53242c39fa92539e6c53be32db283bed869
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-04-22 21:18:11 +0530

    Create LevelWidth.js

---
+++ JSCodingBootcamp/Trees/LevelWidth.js
@@ -0,0 +1,21 @@
+//Given the root node of a tree,return an array where each element is the width of the tree at each level
+/*
+   1
+ / |  \
+2   3  4
+|      |
+5      6
+ ans=[1,3,2]
+*/
+//root is a Node
+function levelWidth(root)
+{
+  const arr=[root,'s'];
+ const widths=[0];
+  while(arr.length>1) //something other than just 's'
+  {
+   const node=arr.shift();
+   
+  }
+
+}
