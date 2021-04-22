ae4d6a08729e60f9d9621ee286c8200889c9834c
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-04-23 00:03:49 +0530

    Update ValidatingBST.js

--- JSCodingBootcamp/Trees/BinarySearchTree/ValidatingBST.js
+++ JSCodingBootcamp/Trees/BinarySearchTree/ValidatingBST.js
@@ -1,5 +1,24 @@
 
-function validate(node)
+function validate(node,min=null,max=null)
 {
+  if(max!=null && node.data>max)
+  {
+    return false; 
+  }
+  if(min!=null&& node.data<min){
+   return false; 
+  }
+  
+  if(node.left && !validate(node.left,min,node.data))
+  {
+   return false; 
+  }
+  if(node.right && !validate(node.right,node.data,max))
+  {
+   return false; 
+  }
+  return true;
   
 }
+//Do level order traversal and keep pushing same level things in array..eventually check if 
+//that array is sorted or not  -O(n) where n=nodes in the tree
