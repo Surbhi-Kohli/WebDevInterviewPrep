ada905036e21bec730a883ff3c04f3d42c8f77e2
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-04-22 19:12:37 +0530

    Update Iterative.js

--- JSCodingBootcamp/Strings/CheckVowels/Iterative.js
+++ JSCodingBootcamp/Strings/CheckVowels/Iterative.js
@@ -3,14 +3,15 @@ function checkVowel(str)
 {
 let count=0;
 const check="aeiou";
-//or
-const check2=['a','e','i','o','u',"substring","abc"]
-//with array thing ,we can add a substring like "abc" to find  a substring like abc
-console.log(check2.includes("substring"));
+//or we can use array
+//const check2=['a','e','i','o','u',"substring","abc"]
+//with array thing ,we can add a substring like "abc" to also find a substring like abc
+//console.log(check2.includes("substring"));//true
 for(let i=0;i<str.length;i++)
-{
+{  
   if(check.includes(str[i])) //inorder to avoid str[i]=='a'||.....condition
   count++;
+  //with array check2.includes(str[i])
 }
 return count;
 }
