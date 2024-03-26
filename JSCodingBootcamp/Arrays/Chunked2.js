bc1a51ac99f4ab1c1f3752112acfaea8abfb8674
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2024-03-26 14:19:01 +0530

    Update Chunked2.js

--- JSCodingBootcamp/Arrays/Chunked2.js
+++ JSCodingBootcamp/Arrays/Chunked2.js
@@ -13,3 +13,23 @@ function chunk(array,size){
   return chunked;
   
   }
+
+
+/*slice method in Javascript*/
+Using slice() to convert array-like objects to arrays
+The slice() method is often used with bind() and call() to create a utility method that converts an array-like object into an array.
+
+JS
+Copy to Clipboard
+// slice() is called with `this` passed as the first argument
+const slice = Function.prototype.call.bind(Array.prototype.slice);
+
+function list() {
+  return slice(arguments);
+}
+
+const list1 = list(1, 2, 3); // [1, 2, 3]
+Using slice() on sparse arrays
+The array returned from slice() may be sparse if the source is sparse.
+
+console.log([1, 2, , 4, 5].slice(1, 4)); // [2, empty, 4]
