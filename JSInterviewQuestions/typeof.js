b863e53d0c7553b9b963ef46f14c707524d8e7e9
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2024-05-02 23:06:30 +0530

    Update typeof.js

--- JSInterviewQuestions/typeof.js
+++ JSInterviewQuestions/typeof.js
@@ -2,14 +2,14 @@ function employeeId() {
 		console.log(typeof employeeId);
 	}
  
-console.log(typeof employeeId); //function
+console.log(typeof employeeId); //"function"
 let abc="ncjdn";
-console.log(typeof abc);//string
-console.log(typeof undeclaredVar);//undefined
+console.log(typeof abc);// "string"
+console.log(typeof undeclaredVar);//"undefined"
 let nullVar=null;
-console.log(typeof nullVar);//Object
+console.log(typeof nullVar);//"Object"
 let numberVar=11;
-console.log(typeof numberVar);//number
+console.log(typeof numberVar);//"number"
 
 /***********************************************/
 var employeeId = 'abc123';
