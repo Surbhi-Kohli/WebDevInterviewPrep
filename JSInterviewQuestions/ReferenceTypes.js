b8e6f200dd8ceca7c918480e3782802003a3ffe3
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2024-05-03 16:02:35 +0530

    Update ReferenceTypes.js

--- JSInterviewQuestions/ReferenceTypes.js
+++ JSInterviewQuestions/ReferenceTypes.js
@@ -7,8 +7,10 @@ arrB[0]=42;
 console.log(arrA)
 /*Ans-The output will be [42,1,2,3,4,5].
 
-Arrays are object in JavaScript and they are passed and assigned by reference. This is why both arrA and arrB point to the same array [0,1,2,3,4,5].
-That's why changing the first element of the arrB will also modify arrA: it's the same array in the memory.
+Arrays are object in JavaScript and they are passed and assigned by reference. 
+This is why both arrA and arrB point to the same array [0,1,2,3,4,5].
+That's why changing the first element of the arrB will also modify
+arrA: it's the same array in the memory.
 */
 
 /************Question 2 *********************************/
@@ -28,7 +30,8 @@ arrB[0].prop1=42;
 console.log(arrA);
 /*
 The output will be [{prop1: 42}, {someProp: "also value of array A!"}, 3,4,5].
-Arrays are object in JS, so both varaibles arrA and arrB point to the same array. Changing arrB[0] is the same as changing arrA[0]*/
+Arrays are object in JS, so both varaibles arrA and arrB point to the same array. 
+Changing arrB[0] is the same as changing arrA[0]*/
 
 /*********************************************************Question 4*************************************************************/
 var arrA = [{prop1: "value of array A!!"}, {someProp: "also value of array A!"},3,4,5];
@@ -39,7 +42,8 @@ console.log(arrA);
 
 /*The output will be [{prop1: 42}, {someProp: "also value of array A!"}, 3,4,5].
 
-The slice function copies all the elements of the array returning the new array. However, it doesn't do deep copying. Instead
+The slice function copies all the elements of the array returning the new array.
+However, it doesn't do deep copying. Instead
 it does shallow copying. You can imagine slice implemented like this:*/
 
 function slice(arr) {
@@ -53,7 +57,8 @@ function slice(arr) {
 Look at the line with result.push(arr[i]). If arr[i] happens to be a number or string,
 it will be passed by value, in other words, copied. If arr[i] is an object, it will be passed by reference.
 
-In case of our array arr[0] is an object {prop1: "value of array A!!"}. Only the reference to this object will be copied. 
+In case of our array arr[0] is an object {prop1: "value of array A!!"}. 
+Only the reference to this object will be copied. 
 This effectively means that arrays arrA and arrB share first two elements.
 
 This is why changing the property of arrB[0] in arrB will also change the arrA[0].*/
